c96e4a8f74deadb51f1e340a523e5b973c58c303
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-04-22 18:39:40 +0300

    lsn 3 done 1/3
    (зробив переключалки між задачами)

--- src/header/PagesHeader.js
+++ src/header/PagesHeader.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+import './PagesHeader.css';
+import { PagesEnum } from "./pages-enum";
 
+const PagesHeader = ({ setChoice }) => {
+    const [selectedPage, setSelectedPage] = useState(null);
 
-
-
-import {PagesEnum} from "./pages-enum";
-const PagesHeader = ({setChoice}) => {
+    const handleButtonClick = (page) => {
+        if (selectedPage !== page) {
+            setSelectedPage(page);
+            setChoice(page);
+        } else {
+            setSelectedPage(null);
+            setChoice(null);
+        }
+    };
 
     return (
-        <div>
+        <div className={'headerDiv'}>
             <div className={'Header'}>
-                <button onClick={() => setChoice(PagesEnum.USERS)}>Users</button>
-                <button onClick={() => setChoice(PagesEnum.COMMENTS)}>Comments</button>
-                <button onClick={() => setChoice(PagesEnum.CARS)}>Cars</button>
+                <button className={'headerButton'} onClick={() => handleButtonClick(PagesEnum.USERS)}>Users</button>
+                <button className={'headerButton'} onClick={() => handleButtonClick(PagesEnum.COMMENTS)}>Comments</button>
+                <button className={'headerButton'} onClick={() => handleButtonClick(PagesEnum.CARS)}>Cars</button>
             </div>
         </div>
     );
 };
 
 export default PagesHeader;
+
+
+
+
+
+// import React from 'react';
+//
+// import './PagesHeader.css';
+// import {PagesEnum} from "./pages-enum";
+//
+//
+//
+// const PagesHeader = ({setChoice}) => {
+//
+//     return (
+//         <div className={'headerDiv'}>
+//             <div className={'Header'}>
+//                 <button className={'headerButton'} onClick={() => setChoice(PagesEnum.USERS)}>Users</button>
+//                 <button className={'headerButton'} onClick={() => setChoice(PagesEnum.COMMENTS)}>Comments</button>
+//                 <button className={'headerButton'} onClick={() => setChoice(PagesEnum.CARS)}>Cars</button>
+//             </div>
+//         </div>
+//     );
+// };
+//
+// export default PagesHeader;
