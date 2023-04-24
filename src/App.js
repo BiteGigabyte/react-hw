efd8009e78237778f2a111748766c9c4bfd972e8
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-04-24 19:26:49 +0300

    lsn 4 - done 0/
    (Трохи передивляюся)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import {Link, Route, Routes} from "react-router-dom";
+import Home from "./component/home/Home";
+import Layout from "./component/layout/Layout";
+import About from "./component/about/About";
+import Users from "./component/users/Users";
+import Posts from "./component/posts/Posts";
+import UserDetails from "./component/user-details/UserDetails";
+import PostDetails from "./component/post-details/PostDetails";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <div>
+
+            <div>
+                <h2>menu</h2>
+                <ul>
+                    <li>
+                        <Link to={'/'}>home</Link>
+                    </li>
+                    <li>
+                        <Link to={'/layout'}>layout</Link>
+                    </li>
+                    <li>
+                        <Link to={'/about'}>about</Link>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h2>view</h2>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/layout'} element={<Layout/>}>
+                        <Route path={'users'} element={<Users/>}>
+                            <Route path={':id'} element={<UserDetails/>}/>
+                        </Route>
+                        <Route path={'posts'} element={<Posts/>}>
+                            <Route path={':id'} element={<PostDetails/>} />
+
+                        </Route>
+                    </Route>
+                    <Route path={'/about'} element={<About/>}/>
+                </Routes>
+
+
+            </div>
+        </div>
+    );
+};
 
 export default App;
