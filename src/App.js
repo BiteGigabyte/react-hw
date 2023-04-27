f72a983ef8ba01c8f6dcb54f35dae56e307a3406
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-04-27 04:23:09 +0300

    typescript master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button} from "./Button/Button";
+import {useContext} from "react";
+import {Context} from "./index";
+import {First} from "./First";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const context = useContext(Context);
+    context.users = ['max', 'kokos', 'kesha']
+    return (
+        <div>
+            {/*<Button bgColor={'red'} styles={{color:'blue'}} height={'50px'} id={1234}>*/}
+            {/*save*/}
+            {/*</Button>*/}
+            {/*<Button bgColor={'black'} styles={{color:'snow'}}>update</Button>*/}
+            <First/>
+        </div>
+    );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
+import App from './App';
+export const Context = createContext(null);
+const value = {}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>
+    <Context.Provider value={value}>
+    <App/>
+    </Context.Provider>
 );
-
