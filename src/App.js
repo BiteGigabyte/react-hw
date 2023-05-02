0608699940763679db39cbee1d6bca4ab1737d74
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-05-02 17:40:13 +0300

    lsn 6 (redux)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const fetchCars = () => async (dispatch) => {
+
+    dispatch({type: 'START_LOADING'});
+    let response = await fetch('http://owu.linkpc.net/carsAPI/v1/cars')
+        .then(value => value.json())
+    console.log(response);
+    dispatch({type: 'SET_CARS', payload: response});
+
+
 }
+
+const App = () => {
+
+    let store = useSelector(state => state);
+    let dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchCars());
+        //.....
+
+    }, []);
+
+
+    return (
+        <div>
+            {store.isLoading && <h2>loading...</h2>}
+
+            {
+                store.cars.map(value => <div key={value.id}>{value.brand}</div>)
+            }
+
+        </div>
+    );
+};
 
 export default App;
