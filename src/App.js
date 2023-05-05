e0f29029e131b961140bc938b2a14e0af389e7bd
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-05-05 14:53:04 +0300

    lsn2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Comments from "./components/comments/Comments";
+import FullComment from "./components/fullComment/FullComment";
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    let [chosenComment, setChosenComment] = useState(null);
+    const lift = (obj) => {
+        setChosenComment({...obj});
+    }
+
+    return (
+        <div className={'container'}>
+
+            {/*<IncrementDecrement/>*/}
+            <div className={'leftSide'}>
+                <Comments lift={lift}/>
+
+            </div>
+
+            {
+                chosenComment
+                &&
+                (
+                    <div className="rightSide">
+                        <FullComment value={chosenComment}/>
+                    </div>
+                )
+
+
+            }
+        </div>
+
+    );
+
 }
+
 
 export default App;
