ac65f0783c8258b3d34583f92460937e37a1af47
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-05-08 22:59:47 +0300

    lsn5_useReducer-part1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useReducer} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'INC1':
+            return {...state, count1: state.count1 + 1}
+        case 'DEC1':
+            return {...state, count1:state.count1-1}
+        case 'RESET1':
+            return {...state, count1:action.payload}
+        case 'INC2':
+            return {...state, count2: state.count2 + 1}
+        case 'DEC2':
+            return {...state, count2:state.count2-1}
+        case 'RESET2':
+            return {...state, count2:action.payload}
+    }
+    return {...state}
 }
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, {count1: 0, count2:0});
+    return (
+        <div>
+            <div>{state.count1}</div>
+            <button onClick={() => dispatch({type: 'INC1'})}>inc</button>
+            <button onClick={() => dispatch({type: 'DEC1'})}>dec</button>
+            <button onClick={() => dispatch({type: 'RESET1', payload:25})}>reset</button>
+            <div>{state.count2}</div>
+            <button onClick={() => dispatch({type: 'INC2'})}>inc</button>
+            <button onClick={() => dispatch({type: 'DEC2'})}>dec</button>
+            <button onClick={() => dispatch({type: 'RESET2', payload:25})}>reset</button>
+        </div>
+    );
+};
 
 export default App;
