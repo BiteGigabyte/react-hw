80f5d1b4f65466cf29f30d2b11b0cd8937df4562
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-05-08 22:55:52 +0300

    lsn5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {Users} from "./components/Users";
+import {useEffect, useState} from "react";
+import {Posts} from "./components/Posts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const [flag, setFlag] = useState(true);
+
+
+
+
+    return (
+        <>
+            <div>
+                <Posts flag={flag}/>
+                <button onClick={() => setFlag(prevState => !prevState)}>hide</button>
+            </div>
+        </>
+    );
+};
 
 export default App;
